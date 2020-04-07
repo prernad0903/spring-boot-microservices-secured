4d1e01e2ae551959a4f1d7b332251d2d2368ba88
Prerna Dingwaney <prerna_dingwaney@persistent.co.in>
2020-04-07 16:54:20 +0530

    Server Side SSL Implementation 1-way authentication

--- movie-catalog-service/src/main/java/com/example/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/com/example/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -11,8 +11,18 @@ import java.security.cert.X509Certificate;
 import javax.net.ssl.SSLContext;
 
 import org.apache.http.client.HttpClient;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.BasicHttpClientConnectionManager;
 import org.apache.http.ssl.SSLContextBuilder;
+import org.apache.http.ssl.SSLContexts;
 import org.apache.http.ssl.TrustStrategy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -28,23 +38,49 @@ public class MovieCatalogServiceApplication {
 	@Bean
 	@LoadBalanced
 	public RestTemplate getRestTemplate() {
-		final String allPassword = "changeit";
+		
+		//2 - WAY SSL
+//		final String allPassword = "changeit";
+//		RestTemplate restTemplate = null;
+//
+//		SSLContext sslContext;
+//		try {
+//			sslContext = SSLContextBuilder.create()
+//					.loadKeyMaterial(ResourceUtils.getFile("classpath:keystore.jks"), allPassword.toCharArray(),
+//							allPassword.toCharArray())
+//					.loadTrustMaterial(ResourceUtils.getFile("classpath:truststore.jks"), allPassword.toCharArray())
+//					.build();
+//			HttpClient client = HttpClients.custom().setSSLContext(sslContext).build();
+//			HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+//			requestFactory.setHttpClient(client);
+//			restTemplate = new RestTemplate(requestFactory);
+//		} catch (KeyManagementException | NoSuchAlgorithmException | KeyStoreException | CertificateException
+//				| IOException | UnrecoverableKeyException e) {
+//			restTemplate = new RestTemplate();
+//		}
 		RestTemplate restTemplate = null;
-
+		TrustStrategy acceptingTrustStrategy = (cert, authType) -> true;
 		SSLContext sslContext;
 		try {
-			sslContext = SSLContextBuilder.create()
-					.loadKeyMaterial(ResourceUtils.getFile("classpath:keystore.jks"), allPassword.toCharArray(),
-							allPassword.toCharArray())
-					.loadTrustMaterial(ResourceUtils.getFile("classpath:truststore.jks"), allPassword.toCharArray())
-					.build();
-			HttpClient client = HttpClients.custom().setSSLContext(sslContext).build();
-			HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
-			requestFactory.setHttpClient(client);
+			sslContext = SSLContexts.custom().loadTrustMaterial(null, acceptingTrustStrategy).build();
+			SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslContext,
+					NoopHostnameVerifier.INSTANCE);
+
+			Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder.<ConnectionSocketFactory>create()
+					.register("https", sslsf).register("http", new PlainConnectionSocketFactory()).build();
+
+			BasicHttpClientConnectionManager connectionManager = new BasicHttpClientConnectionManager(
+					socketFactoryRegistry);
+			CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(sslsf)
+					.setConnectionManager(connectionManager).build();
+
+			HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(
+					httpClient);
+
 			restTemplate = new RestTemplate(requestFactory);
-		} catch (KeyManagementException | NoSuchAlgorithmException | KeyStoreException | CertificateException
-				| IOException | UnrecoverableKeyException e) {
-			restTemplate = new RestTemplate();
+
+		} catch (KeyManagementException | NoSuchAlgorithmException | KeyStoreException e) {
+			e.printStackTrace();
 		}
 
 		return restTemplate;
